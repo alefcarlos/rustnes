c0e991ba552de53d666be62153ae047e6763af7a
Alef Carlos <alef.carlos@gmail.com>
2020-10-27 19:18:55 -0300

    feat: Cartridge model

---
+++ rustnes/src/core/cartridge.rs
@@ -0,0 +1,14 @@
+pub struct Cartridge {
+    // PRG-ROM banks
+    pub PRG: [i8],
+    // CHR-ROM banks
+    pub CHR: [i8],
+    // Save RAM
+    pub SRAM: [i8],
+    // mapper type
+    pub Mapper: i8,
+    //mirroring mode
+    pub Mirror: i8,
+    // battery present
+    pub Battery: i8,
+}
